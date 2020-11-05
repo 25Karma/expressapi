588996eb8aaff79f0af6e25bd7b735a0f4dc10f8
Amos Yu <amosyu2000@gmail.com>
2020-11-04 23:37:17 -0500

    cors now only supports https://25karma.xyz and localhost

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,10 +3,8 @@ import expressCors from 'cors';
 export function cors(req, res, next) {
 	const options = {
 		origin: [
-			"https://25karma.github.io",
-      "https://25karma.xyz",
+			"https://25karma.xyz",
 			"http://localhost:3000",
-			"http://127.0.0.1:9000",
 		],
 	};
 	return expressCors(options);
