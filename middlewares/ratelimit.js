2d2f478a65bc0d686663ee3687b7c838552f6b54
Amos Yu <amosyu2000@gmail.com>
2022-08-29 11:10:20 -0400

    added rate limit info to response headers

--- middlewares/ratelimit.js
+++ middlewares/ratelimit.js
@@ -5,6 +5,8 @@ export function ratelimit(requestsPerMinute) {
 		windowMs: 60 * 1000,
 		max: requestsPerMinute,
 		message: {success: false, reason: 'RATELIMITED'},
+		standardHeaders: true,
+		legacyHeaders: false,
 	};
 	return expressRatelimit(options);
 }
