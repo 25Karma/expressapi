a63daf732ffb206711f0d7c37622e1ba5ff1d4e2
Amos Yu <yua25@mcmaster.ca>
2023-09-26 22:08:16 -0400

    fixed regex in slug, z -> Z

--- middlewares/slug.js
+++ middlewares/slug.js
@@ -3,7 +3,7 @@ export function slug(req, res, next) {
 	res.locals.slug = req.params.slug;
 	// Make sure the slug is a valid Minecraft username or UUID
 	// i.e. only contains alphanumeric characters, underscores, and dashes
-	const reUsername = /^[a-zA-z0-9_]{1,16}$/;
+	const reUsername = /^[a-zA-Z0-9_]{1,16}$/;
 	const reUUID = /[0-9a-fA-F-]{36}|[0-9a-fA-F]{32}/
 	if (!(new RegExp(`${reUsername.source}|${reUUID.source}`)).test(res.locals.slug)) {
 		return res.send({success: false, slug: res.locals.slug, reason: 'MOJANG_PLAYER_DNE'});
