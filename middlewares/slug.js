e74df78be1552df0e47e29d51fcf1062f35818de
Amos Yu <yua25@mcmaster.ca>
2023-10-04 22:31:16 -0400

    updated slug regex for UUIDs

--- middlewares/slug.js
+++ middlewares/slug.js
@@ -4,7 +4,7 @@ export function slug(req, res, next) {
 	// Make sure the slug is a valid Minecraft username or UUID
 	// i.e. only contains alphanumeric characters, underscores, and dashes
 	const reUsername = /^[a-zA-Z0-9_]{1,16}$/;
-	const reUUID = /[0-9a-fA-F-]{36}|[0-9a-fA-F]{32}/
+	const reUUID = /^(-*[0-9a-fA-F]){32}-*$/
 	if (!(new RegExp(`${reUsername.source}|${reUUID.source}`)).test(res.locals.slug)) {
 		return res.send({success: false, slug: res.locals.slug, reason: 'MOJANG_PLAYER_DNE'});
 	}
